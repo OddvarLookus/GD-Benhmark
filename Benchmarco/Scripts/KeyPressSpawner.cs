fa9b7fd856a012b30db69724622a416317681029
OddvarLookus <lorenzo1viglietti@gmail.com>
2023-09-19 00:32:48 +0200

    infos

--- Benchmarco/Scripts/KeyPressSpawner.cs
+++ Benchmarco/Scripts/KeyPressSpawner.cs
@@ -5,26 +5,30 @@ public partial class KeyPressSpawner : Node3D
 {
 
 	[Export] PackedScene cubeTemplate;
-	
+	[Export] Label infosLabel;
+
 	public override void _Ready()
 	{
 		GD.Randomize();
 	}
 
+	int cubesSpawned = 0;
 	void SpawnCube()
 	{
 		Vector3 nPos = new Vector3((float)GD.RandRange(-5.0, 5.0), 0f, (float)GD.RandRange(-5.0, 5.0));
 		RigidBody3D nRB = cubeTemplate.Instantiate() as RigidBody3D;
 		AddChild(nRB);
 		nRB.GlobalPosition = GlobalPosition + nPos;
-		
+
+		cubesSpawned += 1;
+		infosLabel.Text = cubesSpawned.ToString();
 	}
 
 	public override void _Process(double delta)
 	{
 		if (Input.IsPhysicalKeyPressed(Key.Space))
 		{
-			GD.Print("ooo");
+			//GD.Print("ooo");
 			SpawnCube();
 		}
 	}
