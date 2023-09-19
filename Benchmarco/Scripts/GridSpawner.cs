6d96acc845b434e5f32bc04e8517386a83bade24
OddvarLookus <lorenzo1viglietti@gmail.com>
2023-09-19 02:27:48 +0200

    grid spawner

--- Benchmarco/Scripts/GridSpawner.cs
+++ Benchmarco/Scripts/GridSpawner.cs
@@ -8,6 +8,8 @@ public partial class GridSpawner : Node3D
 	[Export] float dist;
 	[Export] int rowNum;
 
+	[Export] Label infoLabel;
+
 	int currentIdx = 0;
 
 	void SpawnObj()
@@ -19,11 +21,22 @@ public partial class GridSpawner : Node3D
 			(float)((currentIdx % (rowNum * rowNum)) / rowNum) * dist
 		);
 		nPos += (float)(currentIdx / (rowNum * rowNum * rowNum)) * dist * (float)rowNum * Vector3.Right;
+		
+		Node3D nNode = objTemplate.Instantiate<Node3D>();
+		AddChild(nNode);
+		nNode.GlobalPosition = nPos;
+		currentIdx += 1;
 
+		infoLabel.Text = $"{currentIdx.ToString()}";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (Input.IsPhysicalKeyPressed(Key.Space))
+		{
+			SpawnObj();
+		}
+
 	}
 }
